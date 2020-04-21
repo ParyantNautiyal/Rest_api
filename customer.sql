d5334714e5b4309254841fc82e8318444755eeb5
ParyantNautiyal <63316707+ParyantNautiyal@users.noreply.github.com>
2020-04-21 12:42:16 +0530

    Add files via upload

--- customer.sql
+++ customer.sql
@@ -1,23 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 21, 2017 at 03:16 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: Apr 21, 2020 at 09:11 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `test`
+-- Database: `Rest_api`
 --
 
 -- --------------------------------------------------------
@@ -26,16 +28,43 @@ SET time_zone = "+00:00";
 -- Table structure for table `customer`
 --
 
-CREATE TABLE IF NOT EXISTS `customer` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  `Address` varchar(255) NOT NULL,
-  `Country` varchar(100) NOT NULL,
-  `Phone` int(10) NOT NULL,
-  `Created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Updated_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `customer` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `Address` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `Country` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+  `Phone` bigint(10) NOT NULL,
+  `CHECK_IN` datetime NOT NULL DEFAULT current_timestamp(),
+  `CHECK_OUT` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`Id`, `Name`, `Address`, `Country`, `Phone`, `CHECK_IN`, `CHECK_OUT`) VALUES
+(1, 'PARYANT NAUTIYAL', 'HNO.2 ,BLOCK B,VASANT KUNJ,NEW DELHI-67', 'INDIA', 9087654321, '2020-04-21 11:39:13', '2020-04-22 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
